c9c70468026be1476de46cdf5ba16658a0292745
pm2am <hsrathore.code@gmail.com>
2024-03-09 16:46:04 +0530

    Implementing Company Listing screen with ViewModel

---
+++ app/src/main/java/com/example/stockapp/presentation/company_listing/CompanyListingState.kt
@@ -0,0 +1,10 @@
+package com.example.stockapp.presentation.company_listing
+
+import com.example.stockapp.domain.model.CompanyListing
+
+data class CompanyListingState(
+    val companies: List<CompanyListing> = emptyList(),
+    val isLoading: Boolean = false,
+    val isRefreshing: Boolean = false,
+    val searchQuery: String = ""
+)
---
+++ app/src/main/java/com/example/stockapp/presentation/company_listing/CompanyListingViewModel.kt
@@ -0,0 +1,69 @@
+package com.example.stockapp.presentation.company_listing
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.stockapp.domain.repo.StockRepository
+import com.example.stockapp.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CompanyListingViewModel @Inject constructor(
+    private val repository: StockRepository
+) : ViewModel() {
+
+    var state by mutableStateOf(CompanyListingState())
+
+    private var searchJob: Job? =null
+
+    fun onEvent(event: CompanyListingEvent) {
+        when(event) {
+            is CompanyListingEvent.Refresh -> {
+                getCompanyListing(fetchFromRemote = true)
+            }
+            is CompanyListingEvent.OnSearchQueryChange -> {
+                state = state.copy(searchQuery =  event.query)
+                searchJob?.cancel()
+                searchJob = viewModelScope.launch {
+                    delay(500)
+                    getCompanyListing()
+                }
+            }
+        }
+    }
+
+    fun getCompanyListing(
+        query: String = state.searchQuery.lowercase(),
+        fetchFromRemote: Boolean = false
+    ) {
+        viewModelScope.launch {
+            repository.getCompanyListings(fetchFromRemote, query)
+                .collect { res->
+                    when(res) {
+                        is Resource.Success -> {
+                            res.data?.let {
+                                state = state.copy(
+                                    companies = it
+                                )
+                            }
+                        }
+                        is Resource.Loading -> {
+                            state = state.copy(isLoading = res.isLoading)
+                        }
+                        is Resource.Error -> {
+
+                        }
+                    }
+                }
+        }
+    }
+
+
+}
+
