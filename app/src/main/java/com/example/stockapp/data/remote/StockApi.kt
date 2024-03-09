eb0c1216cb0c776f3e0a71e5091ce83c2e42e4b2
pm2am <hsrathore.code@gmail.com>
2024-03-09 13:39:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/stockapp/data/remote/StockApi.kt
@@ -0,0 +1,19 @@
+package com.example.stockapp.data.remote
+
+
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface StockApi {
+
+    @GET("query?function=LISTING_STATUS")
+    suspend fun getListings(
+        @Query("apikey") apiKey: String
+    ): ResponseBody
+
+    companion object {
+        const val API_KEY = "WQQZHLP99DLKTRFG"
+        const val BASE_URL = "https://alphavantage.co"
+    }
+}
