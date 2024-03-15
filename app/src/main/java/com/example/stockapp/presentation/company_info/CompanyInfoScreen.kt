3b59203d898acda73d3c0eac90ff19c695a83d2a
pm2am <hsrathore.code@gmail.com>
2024-03-15 18:40:37 +0530

    fixing some minor issue

--- app/src/main/java/com/example/stockapp/presentation/company_info/CompanyInfoScreen.kt
+++ app/src/main/java/com/example/stockapp/presentation/company_info/CompanyInfoScreen.kt
@@ -87,7 +87,7 @@ fun ComposeInfoScreen(
                 if(state.stockInfo.isNotEmpty()) {
                     Spacer(modifier = Modifier.height(16.dp))
                     Text(text = "Market Summary")
-                    Spacer(modifier = Modifier.height(16.dp))
+                    Spacer(modifier = Modifier.height(32.dp))
                     StockChart(
                         infos = state.stockInfo,
                         modifier = Modifier
